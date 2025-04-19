c374732c37fc5f790d223f48eafeda82589186b6
Suzanne102 <suzanne.chartier1@gmail.com>
2025-04-19 22:35:24 +0200

    my index.html file looks good

--- public/js/site.js
+++ public/js/site.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', function () {
     const navToggle = document.querySelector('.nav-toggle');
-    const desktopNav = document.querySelector('.desktop-nav');
-  
+    const mobileNav = document.querySelector('.mobile-nav');
+ 
     navToggle.addEventListener('click', function () {
       desktopNav.classList.toggle('active');
     });
