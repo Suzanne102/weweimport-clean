4a07c4a43cb4ec179bc54532b41271c215cb7556
Suzanne102 <suzanne.chartier1@gmail.com>
2025-04-20 15:16:21 +0200

    added a java script file to push to AI. I copied from the VISIT US hamburger menu on my ipad.

---
+++ public/js/nav-data.js
@@ -0,0 +1,32 @@
+const sublinks = [
+    {
+      page: 'Wewe Import - Our products',
+      links: [
+        { label: 'sandals.html', icon: 'fas fa-shoe', url: 'sandals.html.html' },
+        { label: 'belts', icon: 'fas fa-home', url: 'belts.html' },
+        { label: 'dog-collars', icon: 'fas fa-home', url: 'dog-collars.html' },
+        { label: 'accessories', icon: 'fas fa-home', url: 'accessories.html' },
+        { label: 'bags', icon: 'fas fa-home', url: 'bags.html' },
+        { label: 'jewelry', icon: 'fas fa-home', url: 'jewelry.html' },
+        { label: 'kikois', icon: 'fas fa-home', url: 'kikois.html' },
+      ],
+    },
+    {
+      page: 'gen-info',
+      links: [
+        { label: 'general info', icon: 'fas fa-calendar', url: 'gen-info.html' },
+        { label: 'shop location', icon: 'fas fa-map', url: 'location.html' },
+        { label: 'featured products', icon: 'fas fa-book', url: 'featured-products.html' },
+        { label: 'enquiry form', icon: 'fas fa-book', url: 'enquiry-form.html' },
+      ],
+    },
+    {
+      page: 'more-info',
+      links: [
+        { label: 'about us', icon: 'fas fa-briefcase', url: 'about-us.html' },
+        { label: 'reviews', icon: 'fas fa-briefcase', url: 'reviews.html' },
+      ],
+    },
+  ];
+  
+  export default sublinks;
